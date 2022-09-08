ec1b9c5029bad30db466cbf1320fce5f09dc8712
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-09-08 12:21:44 +0300

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-Product.cerate(
+Product.create(
       "title": "Gold Bracelet with diamonds",
       "price": "20",
       "description": "Beautiful layered bracelet. Perfect accessory for an event.",
@@ -10,4 +10,13 @@ Product.cerate(
       "category": "jewelery",
       "id": 1   
     )
+
+Product.create(
+    "title": "Necklace set",
+    "price": "30",
+    "description": "Matching Gold rings and necklace set. ",
+    "image": "https://images.pexels.com/photos/4259093/pexels-photo-4259093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "category": "jewelery",
+    "id": 2
+)
 puts "✅ Done seeding!"
