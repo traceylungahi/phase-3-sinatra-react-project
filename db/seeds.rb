8bc24361dda29317beb6a69ef39783fb7c3d5b60
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-09-08 12:29:05 +0300

    Update seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Product.create(
       "title": "Gold Bracelet with diamonds",
       "price": "20",
       "description": "Beautiful layered bracelet. Perfect accessory for an event.",
-      "image": "https://images.pexels.com/photos/3641056/pexels-photo-3641056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "image_url": "https://images.pexels.com/photos/3641056/pexels-photo-3641056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       "category": "jewelery",
       "id": 1   
     )
@@ -15,8 +15,26 @@ Product.create(
     "title": "Necklace set",
     "price": "30",
     "description": "Matching Gold rings and necklace set. ",
-    "image": "https://images.pexels.com/photos/4259093/pexels-photo-4259093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "image_url": "https://images.pexels.com/photos/4259093/pexels-photo-4259093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "category": "jewelery",
     "id": 2
+)
+
+Product.create(
+    "title": "Cuban link",
+    "price": "60",
+    "description": "Cuban link chain with diamonds",
+    "image_url": "https://images.pexels.com/photos/12026053/pexels-photo-12026053.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "category": "jewelery",
+    "id": 3
+)
+
+Product.create(
+    "title": "Gold necklace",
+    "price": "15",
+    "description": "Cute dainty necklace with flower pendant",
+    "image_url": "https://images.pexels.com/photos/12145316/pexels-photo-12145316.jpeg",
+    "category": "jewelery",
+    "id": 4
 )
 puts "✅ Done seeding!"
